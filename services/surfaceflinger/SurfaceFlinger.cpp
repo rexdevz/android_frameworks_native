0f76d5ddce4018e54f60e3b25ab66ba0ec1e79de
Jon West <electrikjesus@gmail.com>
2020-03-18 11:51:29 -0400

    Freedom windows: fix drag [2/2]
    Revert "Revert "Fix drag and drop (2/3)""
    
    This reverts commit 5845eafaad804ffe8088cd34cdb52f6f9f9282de.
    
    Change-Id: I6f38f7a0324d7e226e9dcfcc85214f798073ddc7
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3728,7 +3728,7 @@ void SurfaceFlinger::updateInputFlinger() {
         setInputWindowsFinished();
     }
 
-    mInputWindowCommands.clear();
+    executeInputWindowCommands();
 }
 
 void SurfaceFlinger::updateInputWindowInfo() {
@@ -3750,6 +3750,19 @@ void SurfaceFlinger::updateInputWindowInfo() {
 void SurfaceFlinger::commitInputWindowCommands() {
     mInputWindowCommands = mPendingInputWindowCommands;
     mPendingInputWindowCommands.clear();
+}
+
+void SurfaceFlinger::executeInputWindowCommands() {
+    for (const auto& transferTouchFocusCommand : mInputWindowCommands.transferTouchFocusCommands) {
+        if (transferTouchFocusCommand.fromToken != nullptr &&
+            transferTouchFocusCommand.toToken != nullptr &&
+            transferTouchFocusCommand.fromToken != transferTouchFocusCommand.toToken) {
+            mInputFlinger->transferTouchFocus(transferTouchFocusCommand.fromToken,
+                                              transferTouchFocusCommand.toToken);
+        }
+    }
+
+    mInputWindowCommands.clear();
 }
 
 void SurfaceFlinger::updateCursorAsync()

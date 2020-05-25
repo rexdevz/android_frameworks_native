9ff7ced3a8c8079816843db28e5b5a8df3f86bf6
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 18:23:49 +0800

    sf: Exclude VDS use of HWC on non-UM platforms
    Change-Id: Ic0f314f4053628667a921951f610839f36a5079c
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -3502,6 +3502,7 @@ void SurfaceFlinger::processDisplayChangesLocked() {
                             ALOGE_IF(status != NO_ERROR, "Unable to query format (%d)", status);
                             auto format = static_cast<ui::PixelFormat>(intFormat);
 
+#ifdef QCOM_UM_FAMILY
                             if (maxVirtualDisplaySize == 0 ||
                                 ((uint64_t)width <= maxVirtualDisplaySize &&
                                 (uint64_t)height <= maxVirtualDisplaySize)) {
@@ -3514,6 +3515,10 @@ void SurfaceFlinger::processDisplayChangesLocked() {
                                      getHwComposer().allocateVirtualDisplay(width, height, &format);
                                 }
                             }
+#else
+                            displayId =
+                                    getHwComposer().allocateVirtualDisplay(width, height, &format);
+#endif
                         }
 
                         // TODO: Plumb requested format back up to consumer
@@ -4261,12 +4266,14 @@ bool SurfaceFlinger::doComposeSurfaces(const sp<DisplayDevice>& displayDevice,
                 }
                 case Hwc2::IComposerClient::Composition::CLIENT: {
                     renderengine::LayerSettings layerSettings;
+#ifdef QCOM_UM_FAMILY
                     if (displayDevice->isVirtual() &&
                         skipColorLayer(layer->getTypeId())) {
                         // We are not using h/w composer.
                         // Skip color (dim) layer for WFD direct streaming.
                         continue;
                     }
+#endif
                     bool prepared =
                             layer->prepareClientLayer(renderArea, clip, clearRegion,
                                                       renderEngine.isProtected(), layerSettings);
@@ -7434,6 +7441,7 @@ void SurfaceFlinger::setAllowedDisplayConfigsInternal(const sp<DisplayDevice>& d
     setPreferredDisplayConfig();
 }
 
+#ifdef QCOM_UM_FAMILY
 bool SurfaceFlinger::canAllocateHwcDisplayIdForVDS(uint64_t usage) {
 #ifdef QCOM_UM_FAMILY
     uint64_t flag_mask_pvt_wfd = ~0;
@@ -7456,6 +7464,11 @@ bool SurfaceFlinger::canAllocateHwcDisplayIdForVDS(uint64_t usage) {
     return false;
 #endif
 }
+#else
+bool SurfaceFlinger::canAllocateHwcDisplayIdForVDS(uint64_t) {
+    return true;
+}
+#endif
 
 bool SurfaceFlinger::skipColorLayer(const char* layerType) {
     return (sDirectStreaming && !strncmp(layerType, "ColorLayer", strlen("ColorLayer")));
